8cc2f617bbc940c648bc701e7f6df61d7dc49b44
jjinleee <dlwls6877@naver.com>
2024-11-26 12:59:47 +0900

    [feat] #3- CancelButton 구현

--- LazyCat/LazyCat/Components/CancelButton.swift
+++ LazyCat/LazyCat/Components/CancelButton.swift
@@ -1,24 +1,30 @@
 //
-//  LayputButton.swift
+//  MainButton.swift
 //  LazyCat
 //
 //  Created by jinlee on 11/4/24.
 //
 import SwiftUI
 
-struct LoginButton: View {
+struct CancelButton: View {
     @State private var isPressed: Bool = false // 버튼이 눌린 상태를 추적
     
     var body: some View {
         Button(action: {
         }) {
-            Text("Login")
+            Text("Cancel")
                 .font(.system(size: 15, weight: .semibold))
                 .foregroundColor(Color("dark"))
                 .frame(width: 266, height: 49)
-                .background(isPressed ? Color("babyPink") : Color("gray")  ) // 배경색 변경
-                .cornerRadius(10) // 모서리 둥글게
+                .background(isPressed ? Color("babyPink") : Color("white"))
+                .cornerRadius(15)
+
         }
+        .background(Color.white)
+        .overlay(
+                    RoundedRectangle(cornerRadius: 15)
+                        .stroke(Color("dark"), lineWidth: 1) // 테두리
+                )
         .simultaneousGesture(
             DragGesture(minimumDistance: 0)
                 .onChanged({ _ in self.isPressed = true }) // 눌렀을 때 상태 업데이트
@@ -30,12 +36,13 @@ struct LoginButton: View {
 }
 
 // MARK: - Preview
-struct LoginButton_Previews: PreviewProvider {
+struct CancelButton_Previews: PreviewProvider {
     static var previews: some View {
         VStack(spacing: 20) {
-            LoginButton()
+            CancelButton()
         }
         .padding()
         .previewLayout(.sizeThatFits)
     }
 }
+
--- LazyCat/LazyCat/Components/MainButton.swift
+++
@@ -1,7 +0,0 @@
-//
-//  MainButton.swift
-//  LazyCat
-//
-//  Created by jinlee on 11/4/24.
-//
-
